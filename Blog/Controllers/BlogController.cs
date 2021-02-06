e4312d21fa68adc292013c2059df4ac2ad327ec1
Bu9 <s1011550@gmail.com>
2021-02-06 17:14:04 +0800

    initial site

---
+++ Blog/Controllers/BlogController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return string.Empty;
+        }
+    }
+}
