e094f08c0786a6f884fe6433a97e41b1ce13cfcc
Bu9 <s1011550@gmail.com>
2021-02-10 20:12:33 +0800

    sidebar完成

--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -1,32 +1,65 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+   
+   <sidebar-menu
+    :menu="menu"
+    :collapsed="collapsed"
+    :show-one-chile="true"
+    @toggle-collapse="onToggleCollapse"
+    @item-click="onItemClick"
+   />
+   <div
+    v-if="isOnMobile && !collapsed"
+    class="sidebar-overlay"
+    @click="collapsed = true"
+  />
   </div>
 </template>
 
+<script>
+export default {
+  name:'App',
+  data(){
+    return{
+      menu:[
+        {
+          header:true,
+          title:document.title,
+          hiddenOnCollapse:true
+        }
+      ],
+      collapsed:false,
+      isOnMobile:false
+    }
+  },
+  mounted (){
+    this.onResize;
+    window.addEventListener('resize', this.onResize);
+  },
+  methods:{
+    onToggleCollapse(collapsed){
+      console.log(collapsed);
+      this.collapsed = collapsed
+    },
+    onItemClick(event,item,node){
+      console.log('onItemClock');
+      console.log(event);
+      console.log(item);
+      console.log(node);
+    },
+    onResize(){
+      this.isOnMobile = this.collapsed = window.innerWidth <= 767;
+
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,600');
+.v-sidebar-menu .vsm--toggle-btn::after {
+  font-family: 'Font Awesome 5 Pro';
+  contain: '\f337';
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
--- blog-client/src/main.js
+++ blog-client/src/main.js
@@ -1,8 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import VueSidebarMenu from "vue-sidebar-menu";
+
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue/dist/bootstrap-vue.css";
+import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
 
 Vue.config.productionTip = false;
+
+// Make BootstrapVue available throughout your project
+Vue.use(BootstrapVue);
+// Optionally install the BootstrapVue icon components plugin
+Vue.use(IconsPlugin);
+// vue-sidebar
+Vue.use(VueSidebarMenu);
 
 new Vue({
   router,
