78cc245ba7fd85319b793330e0de706bf18d2389
Bu9 <s1011550@gmail.com>
2021-02-10 23:27:39 +0800

    加routes以及版面色系調整

--- blog-client/src/App.vue
+++ blog-client/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-   
+    <div id="RouterView" :class="[{'collapsed':collapsed},{'onmobile' : isOnMobile}]">
+      <router-view />
+    </div>
    <sidebar-menu
     :menu="menu"
     :collapsed="collapsed"
@@ -23,9 +25,9 @@ export default {
     return{
       menu:[
         {
-          header:true,
+          href:'/',
           title:document.title,
-          hiddenOnCollapse:true
+          icon:'fas fa-home'
         },
         {
           href:'/SomethingWrong',
@@ -33,8 +35,8 @@ export default {
           icon:'fas fa-stethoscope'
         },
         {
-          href:'/Recipt',
-          title:'食譜 Recipt',
+          href:'/Recipe',
+          title:'食譜 Recipe',
           icon:'fas fa-torah'
         },
         {
@@ -63,6 +65,7 @@ export default {
       console.log(node);
     },
     onResize(){
+      console.log(process.env.BASE_URL)
       this.isOnMobile = this.collapsed = window.innerWidth <= 767;
 
     }
@@ -71,8 +74,42 @@ export default {
 </script>
 
 <style>
+html,body{
+  margin: 0;
+  padding: 0;
+}
 
 .v-sidebar-menu .vsm--title {
   color: #ffa600;
+}
+
+.v-sidebar-menu .vsm--link.vsm--link_active {
+  color: #ffa600;
+  box-shadow: 3px 0px 0px 0px #ffa600 inset !important;
+  -webkit-box-shadow:3px 0px 0px 0px #2a2a2e inset !important;
+}
+/*
+.v-sidebar-menu .vsm--link.vsm--link_hover{
+background-color: #ffa600;
+} */
+
+.v-sidebar-menu .vsm--mobile-bg{
+  background-color: #2a2a2e !important;
+}
+
+.v-sidebar-menu.vsm_collapsed .vsm--link_level-1.vsm--link_hover .vsm--icon, .v-sidebar-menu.vsm_collapsed .vsm--link_level-1:hover .vsm--icon{
+  background-color: #ffa600  !important; 
+}
+
+#RouterView{
+  padding-left: 350px;
+  transition: 0.3s ease;
+}
+
+#RouterView.collapsed {
+  padding-left: 50px;
+}
+#RouterView.onmobile {
+  padding-left: 50px;
 }
 </style>
--- blog-client/src/router/index.js
+++ blog-client/src/router/index.js
@@ -1,9 +1,35 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 
+import HomePage from "../components/HomePage.vue";
+import TechnicalNote from "../components/TechnicalNote.vue";
+import Recipe from "../components/Recipe.vue";
+import Interview from "../components/Interview.vue";
+
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: HomePage,
+  },
+  {
+    path: "/SomethingWrong",
+    name: "TechnicalNote",
+    component: TechnicalNote,
+  },
+  {
+    path: "/Recipe",
+    name: "Recipe",
+    component: Recipe,
+  },
+  {
+    path: "/Interview",
+    name: "Interview",
+    component: Interview,
+  },
+];
 
 const router = new VueRouter({
   mode: "history",
