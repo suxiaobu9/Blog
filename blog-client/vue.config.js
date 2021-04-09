baae8f5ba527a933f1a8cc3c233d77cf068b6d04
Bu9 <s1011550@gmail.com>
2021-04-09 14:34:49 +0800

    前端分組態

--- blog-client/vue.config.js
+++ blog-client/vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   chainWebpack: (config) => {
     config.plugin("html").tap((args) => {
-      args[0].title = " 菜鳥工程師的通靈之旅 ";
+      args[0].title = " Ovre Loading ";
       return args;
     });
   },
